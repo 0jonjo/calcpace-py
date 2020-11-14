893602852e27e41e2de5776408eb4ac583411c74
0jonjo <jgilbertons@gmail.com>
2020-11-14 15:06:18 -0300

    Second commit

--- calcpace.py
+++ calcpace.py
@@ -1,13 +1,12 @@
 dist = input('Qual a distância percorrida em km? Ex: X.X ')
-tempo = input('Quanto tempo? Ex: HH:MM:SS?' )
+tempo = input('Quanto tempo (H:M:S)? ' )
 hora, minuto, segundo = tempo.split(':')
-h, m, s = 0, 0, 0
+m, s = 0, 0
 
 t = (int(hora)*60*60) + (int(minuto)*60) + int(segundo)
 p = t / float(dist)
-if p > 60:
+while p >= 60:
     m += 1
-else:
-    s = p
-    
-print(f'Você percorreu os {dist}km no pace {m}:{s}.')
+    p = p - 60
+p = round(p)
+print(f'Você percorreu os {dist}km no pace {m}:{p}.')
