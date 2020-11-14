24da1638a3223394d0b314c79a12397c830a7c84
0jonjo <jgilbertons@gmail.com>
2020-11-14 19:40:34 -0300

    Readme andcalpace function

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # calcpace
-A simple python project to calculate a run pace or predict a run time (marathon, half-marathon, 10 and 5km..).
+[EN]
+
+A simple Python project to calculate a run pace or a predict run time (marathon, half-marathon, 10 and 5km...)
+
+Option 1 - PACE
+Put a run time (HH:MM:SS) and distance (XX.XX km)
+It returns a pace (MM:SS)
+
+Option 2 - RUN TIME - 't' option 
+Put a pace (MM:SS) and distance (XX.XX km)
+It returns a predict run time (HH:MM:SS)
+
+[PT-BR]
+
+Um projeto simples em Python para calcular o pace de corrida ou um tempo de corrida previsto  (maratona, meia-maratona, 10 e 5km...) 
+
+Opção 1 - PACE
+Coloque o tempo de corrida (HH:MM:SS) and distancia (XX.XX km)
+Ele retorna o pace (MM:SS)
+
+Opção 2 - Tempo de corrida - opção 't'
+Coloque um pace (MM:SS) e a distancia (XX.XX km)
+Ele retorna o tempo de corrida previsto (HH:MM:SS)
+
+-x-
+
+0jonjo @mfornaciari
--- calcpace.py
+++ calcpace.py
@@ -1,12 +1,32 @@
-dist = input('Qual a distância percorrida em km? Ex: X.X ')
-tempo = input('Quanto tempo (H:M:S)? ' )
-hora, minuto, segundo = tempo.split(':')
-m, s = 0, 0
+escolha = input("Digite 't' para prever um tempo de corrida\n ou qualquer tecla para calcular o pace:")
+h, m, s = 0, 0, 0
 
-t = (int(hora)*60*60) + (int(minuto)*60) + int(segundo)
-p = t / float(dist)
-while p >= 60:
-    m += 1
-    p = p - 60
-p = round(p)
-print(f'Você percorreu os {dist}km no pace {m}:{p}.')
+if escolha == 't':
+    
+    dist = float(input('Qual a distância a ser percorrida em km? Ex: X.X '))
+    pace = input('Qual o pace desejado (M:S)? ' )
+    minuto, segundo = pace.split(':')
+    m = dist * int(minuto)
+    s = dist * int(segundo)
+    while s >= 60:
+            m += 1
+            s -= 60
+            while m >= 60:
+                h += 1
+                m -= 60
+    h, m, s = round(h), round(m), round(s)
+    print(f'Você percorrerá os {dist}km no tempo de {h:0>2}h{m:0>2}min{s:0>2}seg.')
+
+else:
+    
+    dist = input('Qual a distância percorrida em km? Ex: X.X ')
+    tempo = input('Quanto tempo (H:M:S)? ' )
+    hora, minuto, segundo = tempo.split(':')
+    
+    t = (int(hora)*3600) + (int(minuto)*60) + int(segundo)
+    p = t / float(dist)
+    while p >= 60:
+        m += 1
+        p -= 60
+    p = round(p)
+    print(f'Você percorreu os {dist}km no pace {m}:{p:0>2}.')
