ed2649d307911ab5bbc73d59d2324af102edfad9
0jonjo <jgilbertons@gmail.com>
2020-11-29 16:07:14 -0300

    marathon and half options

--- calcpace.py
+++ calcpace.py
@@ -4,22 +4,32 @@ escolha = input("Digite 't' para prever um tempo de corrida\n ou qualquer tecla 
 
 if escolha == 't':
 
-    dist = float(input('Qual a distância a ser percorrida em km? Ex: X.X '))
+    dist = input('Qual a distância a ser percorrida em km?\n m = maratona, h = meia maratona ou digite os km. Ex: X.X ')
     pace = input('Qual o pace desejado (M:S)? ')
     minuto, segundo = pace.split(':')
     t = int(minuto) * 60 + int(segundo)
-    d = dist * t
+    if dist == 'm':
+        d = t * 42.195
+    if dist == 'h':
+        d = t * 21.0975
+    else:
+        d = float(dist) * t
     ty_res = time.gmtime(d)
     tempo_total = time.strftime("%H:%M:%S",ty_res)
-    print(f'Você percorrerá os {dist}km no tempo de {tempo_total}.')
+    print(f'Você percorrerá a distância no tempo de {tempo_total}.')
     
 else:
 
-    dist = float(input('Qual a distância a ser percorrida em km? Ex: X.X '))
+    dist = input('Qual a distância a ser percorrida em km?\n m = maratona, h = meia maratona ou digite os km. Ex: X.X ')
     tempo = input('Quanto tempo (H:M:S)? ')
     hora, minuto, segundo = tempo.split(':')
     t = (int(hora)*3600) + (int(minuto)*60) + int(segundo)
-    p = t / dist
+    if dist == 'm':
+        p = t / 42.195
+    if dist == 'h':
+        p = t / 21.0975
+    else:
+        p = t / float(dist)
     ty_res = time.gmtime(p)
     pace = time.strftime("%M:%S",ty_res)
-    print(f'Você percorreu os {dist}km no pace {pace}.')
+    print(f'Você percorreu a distância no pace {pace}.')
