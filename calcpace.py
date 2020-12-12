47a1d174b8e985698998e996adc9dc7ab97ca096
0jonjo <jgilbertons@gmail.com>
2020-12-12 15:50:37 -0300

    calcpace version 2

--- calcpace.py
+++ calcpace.py
@@ -1,35 +1,28 @@
 import time
 
-escolha = input("Digite 't' para prever um tempo de corrida\n ou qualquer tecla para calcular o pace: ")
-
-if escolha == 't':
-
-    dist = input('Qual a distância a ser percorrida em km?\n m = maratona, h = meia maratona ou digite os km. Ex: X.X ')
-    pace = input('Qual o pace desejado (M:S)? ')
-    minuto, segundo = pace.split(':')
-    t = int(minuto) * 60 + int(segundo)
-    if dist == 'm':
-        d = t * 42.195
-    if dist == 'h':
-        d = t * 21.0975
-    else:
-        d = float(dist) * t
-    ty_res = time.gmtime(d)
-    tempo_total = time.strftime("%H:%M:%S",ty_res)
-    print(f'Você percorrerá a distância no tempo de {tempo_total}.')
-    
+#Escolha da distância base para os cálculos
+dist = input('Qual a distância a ser percorrida em km?\n m = maratona, h = meia maratona ou digite os km. Ex: X.X ')
+if dist == 'm':
+    c = 42.195
+if dist == 'h':
+    c = 21.0975
 else:
+    c = float(dist)
+    
+#Input do tempo total de prova ou do pace
+tempo = input('Quanto tempo ou pace. Escreva no formato (H:M:S)? ')
+hora, minuto, segundo = tempo.split(':')
+t = (int(hora)*3600) + (int(minuto)*60) + int(segundo)
 
-    dist = input('Qual a distância a ser percorrida em km?\n m = maratona, h = meia maratona ou digite os km. Ex: X.X ')
-    tempo = input('Quanto tempo (H:M:S)? ')
-    hora, minuto, segundo = tempo.split(':')
-    t = (int(hora)*3600) + (int(minuto)*60) + int(segundo)
-    if dist == 'm':
-        p = t / 42.195
-    if dist == 'h':
-        p = t / 21.0975
-    else:
-        p = t / float(dist)
-    ty_res = time.gmtime(p)
-    pace = time.strftime("%M:%S",ty_res)
-    print(f'Você percorreu a distância no pace {pace}.')
+#Escolha da opção de calculo - 0 para tempo de corrida / qualquer tecla para pace    
+escolha = input("Digite '0' para prever um tempo de corrida\n ou qualquer tecla para calcular o pace: ")
+if escolha == '0':
+    d = t * c
+else:
+    d = t / c
+    
+#Padronização de formato e impressão do resultado de acordo com a escolha do usuário
+ty_res = time.gmtime(d)
+resultado = time.strftime("%H:%M:%S",ty_res)
+print(f'Você percorrerá em {resultado}.')
+    
