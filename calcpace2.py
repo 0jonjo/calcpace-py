171cbe064693cbaed65df021d07adf8e8fb19a06
jonjo <joaogilberto@tuta.io>
2021-05-02 22:17:29 -0300

    Implementacao Gravar Corrida

--- calcpace2.py
+++ calcpace2.py
@@ -1,24 +1,40 @@
 import time
 from functions import dist
 from functions import tempo
+from functions import ajustetempo
 from functions import pace
 from functions import prevcorrida
+from functions import convertertempo
 from functions import impressao
+from functions import entradadata
+from functions import observacao
+from functions import lerdados
+from functions import escreverdados
 
 #Menu inicial
 opcao = input('Escolha a opção desejada: \n [1. CALC PACE] [2. CALC TEMPO CORRIDA]\n [3. SALVAR CORRIDA] [4. VER/EDITAR CORRIDA]\n')
 if opcao == '1':
     totaldistancia = dist()
     totaltempo = tempo()
-    finalpace = pace(totaltempo, totaldistancia)
-    impress = impressao(finalpace)
+    ajuste = ajustetempo(totaltempo)
+    finalpace = pace(ajuste, totaldistancia)
+    converter = convertertempo(finalpace)
+    impress = impressao(converter)
 elif opcao == '2':
     totaldistancia = dist()
     totaltempo = tempo()
+    ajuste = ajustetempo(totaltempo)
     calctempo = prevcorrida(totaltempo, totaldistancia)
-    impress = impressao(calctempo)
+    converter = convertertempo(finalpace)
+    impress = impressao(converter)
 elif opcao == '3':
-    entradaData = input("Insira a data no formato DD/MM/AAAA: ")
-    data = datetime.strptime(entradaData, "%d/%m/%Y")
+    data = entradadata()
+    totaldistancia = dist()
+    totaltempo = tempo()
+    ajuste = ajustetempo(totaltempo)
+    finalpace = pace(ajuste, totaldistancia)
+    converter = convertertempo(finalpace)           
+    obs = observacao()
+    escrever = escreverdados(str(data), totaldistancia, str(totaltempo), str(converter), obs)  
 else:
-    print('')
+    print('Opção inválida')
--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
 import time
 import datetime
 from datetime import datetime
+import csv
+from csv import reader, writer
 
 def dist():
-    distancia = input('Qual a distância percorrida em km?\n m = maratona, h = meia maratona ou digite os km. Ex: X.X ')
+    distancia = input("Escreva a distância em km?\n m = maratona, h = meia maratona ou digite os km em X.X ")
     if distancia == 'm':
         d = 42.195
     elif distancia == 'h':
@@ -13,7 +15,10 @@ def dist():
     return d
 
 def tempo():
-    temp = input('Escreva o tempo ou pace da corrida formato (H:M:S)? ')
+    temp = input("Escreva o tempo ou pace da corrida formato HH:MM:SS) ")
+    return temp
+    
+def ajustetempo(temp):
     hora, minuto, segundo = temp.split(':')
     t = (int(hora)*3600) + (int(minuto)*60) + int(segundo)
     return t
@@ -26,8 +31,36 @@ def prevcorrida(pacetempo, distancia):
     resultadotempocorrida = float(pacetempo) * float(distancia)
     return resultadotempocorrida
 
-def impressao(d):
-    ty_res = time.gmtime(d)
+def convertertempo(t):
+    ty_res = time.gmtime(t)
     resultado = time.strftime("%H:%M:%S",ty_res)
-    print(f'O resultado é {resultado}.')
+    return resultado
 
+def impressao(i):
+    print(f'O resultado é {i}.')
+
+def entradadata():
+    entradaData = input("Insira a data no formato DD/MM/AAAA: ")
+    data = datetime.strptime(entradaData, "%d/%m/%Y")
+    return data
+
+def observacao():
+    obs = input("Insira a observação: ")
+    return obs
+
+def lerdados():
+    with open('dadoscorrida.csv') as csv_file:
+    
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        for row in csv_reader:
+            print( row[0] + ' - ' + row[1] + ' - ' + row[2] + ' - ' + row[3] + ' - ' + row[4] )
+
+def escreverdados(data, dist, tempo, pace, obs):
+    with open('dadoscorrida.csv', 'a', newline='') as file:
+
+        data, 
+    
+        writer = csv.writer(file)
+    
+        writer.writerow([data, dist, tempo, pace, obs])
