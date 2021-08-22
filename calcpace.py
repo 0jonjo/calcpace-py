79d2cea78daffd5bea7148255ad3b821a50763da
jonjo <joaogilberto@tuta.io>
2021-08-22 10:59:15 -0300

    Translate functions.py

--- calcpace.py
+++ calcpace.py
@@ -1,47 +1,50 @@
 from functions import dist
-from functions import tempo
-from functions import ajustetempo
-from functions import pace
-from functions import prevcorrida
-from functions import convertertempo
-from functions import impressao
-from functions import entradadata
-from functions import observacao
-from functions import inserirdados
-from functions import verdados
-from functions import deletardados
-from functions import criartabela
+from functions import timerun
+from functions import adjust
+from functions import calcpace
+from functions import predictrun
+from functions import converttime
+from functions import toprint
+from functions import date
+from functions import observations
+from functions import setdata
+from functions import getdata
+from functions import deletedata
+from functions import createtable
 
 #Menu inicial
-opcao = input('Escolha a opção desejada:\n [1. CALC PACE] [2. CALC TEMPO CORRIDA]\n [3. SALVAR CORRIDA] [4. DELETAR CORRIDA]\n [5. VER CORRIDAS SALVAS]\n')
-if opcao == '1':
-    totaldistancia = dist()
-    totaltempo = tempo()
-    ajuste = ajustetempo(totaltempo)
-    finalpace = pace(ajuste, totaldistancia)
-    converter = convertertempo(finalpace)
-    impress = impressao(converter)
-elif opcao == '2':
-    totaldistancia = dist()
-    totaltempo = tempo()
-    ajuste = ajustetempo(totaltempo)
-    calctempo = prevcorrida(totaltempo, totaldistancia)
-    converter = convertertempo(finalpace)
-    impress = impressao(converter)
-elif opcao == '3':
-    criartabela()
-    data = entradadata()
-    totaldistancia = dist()
-    totaltempo = tempo()
-    ajuste = ajustetempo(totaltempo)
-    finalpace = pace(ajuste, totaldistancia)
-    converter = convertertempo(finalpace)           
-    obs = observacao()
-    escrever = inserirdados(data, totaldistancia, totaltempo, converter, obs)
-elif opcao == '4':
-    verdados()
-    deletardados()
-elif opcao == '5':
-    verdados()
+print("""
+CALCPACE
+""")
+option = input('Escolha a opção desejada:\n 1. CALCULAR O PACE\n 2. CALCULAR O TEMPO CORRIDA\n 3. SALVAR CORRIDA\n 4. DELETAR CORRIDA\n 5. VER CORRIDAS SALVAS\n Ctrl + C para sair do programa a qualquer momento\n')
+if option == '1':
+    total = dist()
+    totaltime= timerun()
+    adjusted = adjust(totaltime)
+    finalpace = calcpace(adjusted, total)
+    convertedpace = converttime(finalpace)
+    impress = toprint(convertedpace)
+elif option == '2':
+    total = dist()
+    totaltime= timerun()
+    adjusted = adjust(totaltime)
+    calctemp = predictrun(adjusted, total)
+    convertedtemprun = converttime(calctemp)
+    impress = toprint(convertedtemprun)
+elif option == '3':
+    createtable()
+    thedate = date()
+    total = dist()
+    totaltime = timerun()
+    adjusted = adjust(totaltime)
+    finalpace = calcpace(adjusted, total)
+    convertedpace = converttime(finalpace)           
+    obs = observations()
+    escrever = setdata(date, total, totaltime, convertedpace, obs)
+elif option == '4':
+    getdata()
+    deletedata()
+elif option == '5':
+    getdata()
 else:
     print('Opção inválida')
